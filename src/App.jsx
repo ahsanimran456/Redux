f53cefd7c80ebc48bc05da62fb28df2200b0a268
ahsan imran <ahxanimran456@gmail.com>
2024-05-12 17:03:03 +0500

    initialize usereducer hook and callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useReducer, useState } from 'react'
+import React, { useCallback, useEffect, useReducer, useState } from 'react'
 import './App.css'
 import Todos from './Components/Todos';
 
@@ -8,18 +8,50 @@ function App() {
   }
 
   // todoReducer contains current state and action this function will return new state and manage the state ,
-    // this function will be called when ever we dispatch an action 
-  
+  // this function will be called when ever we dispatch an action 
+
   const todoReducer = (state, action) => {
-    
+    switch (action.type) {
+      case 'ADD_TODO':
+        return {
+          todos: [...state.todos, action.payload]
+        }
+      // case 'DELETE_TODO':
+      //   return {
+      //     todos: state.todos.filter((todo, index) => index !== action.payload)
+      //   }
+      default:
+        return state
+    }
+
   }
   const [state, dispatch] = useReducer(todoReducer, init)
+  const [UserInput, setUserInput] = useState("");
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
+  const HandleOnchange = useCallback((e) => {
+    setUserInput(e.target.value)
+  }, [])
+
+
+  const HandleAddTodo = useCallback(() => {
+    dispatch({
+      type: 'ADD_TODO',
+      payload: UserInput
+    })
+    setUserInput("")
+  })
   return (
+    <React.Fragment>
+      <input type="text" placeholder='Enter Todo' onChange={HandleOnchange} />
 
+      <button onClick={HandleAddTodo}>Add Todo </button>
 
-    <div>
-      adsads
-    </div>
+    </React.Fragment>
+
   )
 }
 
